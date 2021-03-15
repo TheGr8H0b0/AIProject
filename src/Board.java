63e3104bac34e03e3a70ac6ac510619f0b220e6a
Rachel Franklin <rachelcfranklin21@gmail.com>
2021-03-14 22:19:54 -0400

    Board.java: added variables, wrote Board constructor, wrote setRandomPegEmpty, wrote pegExists

--- src/Board.java
+++ src/Board.java
@@ -1,3 +1,36 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Board {
+
+    private final int depth = 5;
+    private ArrayList<ArrayList<Boolean>> pegs; //true = peg in this location
+
+    Board(){
+        pegs = new ArrayList<>();
+        for (int i = 0; i < depth; i++){
+            ArrayList <Boolean> row = new ArrayList<>();
+            for (int j = 0; j < i; j++){
+                row.add(true);
+            }
+            pegs.add(row);
+        }
+
+        setRandomPegEmpty();
+    }
+
+    //sets a random peg
+    private void setRandomPegEmpty(){
+        Random rand = new Random();
+        int rowNum = rand.nextInt(depth);
+        int colNum = rand.nextInt(rowNum);
+        pegs.get(rowNum).set(colNum, false);
+    }
+
+    private boolean pegExists(int row, int col){
+        return pegs.get(row).get(col);
+    }
+
+
 
 }
