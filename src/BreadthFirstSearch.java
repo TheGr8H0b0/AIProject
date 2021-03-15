556ffb6d5222df990a10019f51ac6701f47c8cf3
Matthew (A.K.A: TheGr8H0b0) <matthewbennett123@gmail.com>
2021-03-15 15:30:28 -0400

    Little things are great

---
+++ src/BreadthFirstSearch.java
@@ -0,0 +1,13 @@
+package src;
+
+import java.util.Random;
+import java.util.ArrayList;
+import java.lang.Math;
+
+public class BreadthFirstSearch {
+    
+
+    BreadthFirstSearch(Board startBoard) {
+
+    }
+}
---
+++ src/Game.java
@@ -0,0 +1,8 @@
+package src;
+
+public class Game {
+    
+    public static void main(String args[]) {
+        Board newGame = new Board(5);
+    }
+}
