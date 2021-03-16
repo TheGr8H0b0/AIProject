4e55ad4fd55996349c0ca07c5159d11780df131b
Matthew (A.K.A: TheGr8H0b0) <matthewbennett123@gmail.com>
2021-03-15 23:40:16 -0400

    Mini changes to pegNode

--- src/PegSolitaireSolver.java
+++ src/PegSolitaireSolver.java
@@ -18,12 +18,20 @@ public class PegSolitaireSolver {
 
     public class Node {
 
-        public Board currentState;
-        public Board parent;
+        private Board currentState;
+        private Board parent;
     
         public Node(Board state, Board parent){
             currentState = state;
             this.parent = parent;
+        }
+
+        public Board getState() {
+            return currentState;
+        }
+
+        public Board getParent() {
+            return parent;
         }
         
     }
@@ -34,10 +42,33 @@ public class PegSolitaireSolver {
         */
         int[] initEmpty = initialState.getInitialEmpty();
         ArrayList<int []> potentialStartPegs = initialState.getWithinReach(initEmpty[0], initEmpty[1]);
+        Node head = new Node(initialState, null);
 
         for (int i = 0; i < potentialStartPegs.size(); i++) {
-            
+
         }
+    }
+
+    public Node bidirectionalSearchRecursive(Node head) {
+        int numPegs = head.getState().getNumPegs();
+        int depth = head.getState().getDepth();
+
+        if (numPegs == 1) {
+            return head;
+        }
+
+        //Build the list of potential moves from the current state
+        ArrayList<int []> pegJumpList = new ArrayList<int []>();
+        for (int i = 0; i < depth; i++) {
+            for (int j = 0; j <= i; j++) {
+                ArrayList<int []> tempJumpList = initialState.getWithinReach(i, j);
+                for (int k  = 0; k < tempJumpList.size(); k++) {
+                    pegJumpList.add(tempJumpList.get(k));
+                }
+            }
+        }
+
+        return new Node(null, null);
     }
     
 }
